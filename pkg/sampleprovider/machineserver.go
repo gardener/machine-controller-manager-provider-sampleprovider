6efff5e2cca2f842b907be4642b1f65341420dec
prashanth26 <prashanth@sap.com>
2019-02-06 12:12:10 +0530

    Updated documentation

--- pkg/sampleprovider/machineserver.go
+++ pkg/sampleprovider/machineserver.go
@@ -34,9 +34,10 @@ import (
 
 // NOTE
 //
-// The basic working of the controller is works with just implementing the CreateMachine() & DeleteMachine() methods.
-// The other methods help in working of the controller efficently.
-// However, it is recommended that you implement all the methods mentioned below.
+// The basic working of the controller will work with just implementing the CreateMachine() & DeleteMachine() methods.
+// You can first implement these two methods and check the working of the controller.
+// Once this works you can implement the rest of the methods.
+// Implementation of few methods like - ShutDownMachine() are optional, however we highly recommend implementing it as well.
 
 // CreateMachine handles a machine creation request
 // REQUIRED METHOD
@@ -48,14 +49,15 @@ import (
 //
 // RESPONSE PARAMETERS (cmi.CreateMachineResponse)
 // MachineID			string				Unique identification of the VM at the cloud provider. This could be the same/different from req.Name.
-//											MachineID typically matches with the node.Spec.ProviderID on the node object. Eg: gce://project-name/region/vm-machineID
+//											MachineID typically matches with the node.Spec.ProviderID on the node object.
+//											Eg: gce://project-name/region/vm-machineID
 // NodeName				string				Returns the name of the node-object that the VM register's with Kubernetes.
 // 											This could be different from req.Name as well
 //
 // OPTIONAL IMPLEMENTATION LOGIC
-// It is optionally expected by the safety controller to use an identification mechanisms to map the VM Created by a machine class.
+// It is optionally expected by the safety controller to use an identification mechanisms to map the VM Created by a providerSpec.
 // These could be done using tag(s)/resource-groups etc.
-// This logic is ysed by safety controller to delete orphan VMs which are not backed by any machine CRD
+// This logic is used by safety controller to delete orphan VMs which are not backed by any machine CRD
 //
 func (ms *MachineServer) CreateMachine(ctx context.Context, req *cmi.CreateMachineRequest) (*cmi.CreateMachineResponse, error) {
 	// Log messages to track start of request
@@ -77,7 +79,6 @@ func (ms *MachineServer) DeleteMachine(ctx context.Context, req *cmi.DeleteMachi
 }
 
 // GetMachine handles a machine details fetching request
-// OPTIONAL METHOD
 //
 // REQUEST PARAMETERS (cmi.GetMachineRequest)
 // MachineID 		string				Contains the unique identification of the VM at the cloud provider
@@ -93,23 +94,9 @@ func (ms *MachineServer) GetMachine(ctx context.Context, req *cmi.GetMachineRequ
 	return nil, status.Error(codes.Unimplemented, "")
 }
 
-// ShutDownMachine handles a machine shutdown/power-off/stop request
-// OPTIONAL METHOD
-//
-// REQUEST PARAMETERS (cmi.ShutDownMachineRequest)
-// MachineID 		string				Contains the unique identification of the VM at the cloud provider
-// Secrets			map<string,bytes>	(Optional) Contains a map from string to string contains any cloud specific secrets that can be used by the provider
-//
-func (ms *MachineServer) ShutDownMachine(ctx context.Context, req *cmi.ShutDownMachineRequest) (*cmi.ShutDownMachineResponse, error) {
-	// Log messages to track start of request
-	glog.V(2).Infof("ShutDown machine request has been recieved for %q", req.MachineID)
-	return nil, status.Error(codes.Unimplemented, "")
-}
-
 // ListMachines lists all the machines possibilly created by a providerSpec
 // Identifying machines created by a given providerSpec depends on the OPTIONAL IMPLEMENTATION LOGIC
 // you have used to identify machines created by a providerSpec. It could be tags/resource-groups etc
-// OPTIONAL METHOD
 //
 // REQUEST PARAMETERS (cmi.ListMachinesRequest)
 // ProviderSpec		bytes(blob)			Template/Configuration of the machine that wouldn've been created by this ProviderSpec (Machine Class)
@@ -122,6 +109,19 @@ func (ms *MachineServer) ShutDownMachine(ctx context.Context, req *cmi.ShutDownM
 func (ms *MachineServer) ListMachines(ctx context.Context, req *cmi.ListMachinesRequest) (*cmi.ListMachinesResponse, error) {
 	// Log messages to track start of request
 	glog.V(2).Infof("List machines request has been recieved for %q", req.ProviderSpec)
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// ShutDownMachine handles a machine shutdown/power-off/stop request
+// OPTIONAL METHOD
+//
+// REQUEST PARAMETERS (cmi.ShutDownMachineRequest)
+// MachineID 		string				Contains the unique identification of the VM at the cloud provider
+// Secrets			map<string,bytes>	(Optional) Contains a map from string to string contains any cloud specific secrets that can be used by the provider
+//
+func (ms *MachineServer) ShutDownMachine(ctx context.Context, req *cmi.ShutDownMachineRequest) (*cmi.ShutDownMachineResponse, error) {
+	// Log messages to track start of request
+	glog.V(2).Infof("ShutDown machine request has been recieved for %q", req.MachineID)
 	return nil, status.Error(codes.Unimplemented, "")
 }
 
